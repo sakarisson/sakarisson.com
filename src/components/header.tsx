ec7eecc3459583face0be8acd6147d12cd30b2ff
Kristian Sakarisson <kristian@sakarisson.com>
2020-02-17 21:57:46 +0200

    style

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -2,11 +2,6 @@ import { Link } from "gatsby"
 import React from "react"
 import styled from "styled-components"
 
-type Props = {
-  title: string
-  subtitle: string
-}
-
 const Head = styled.header`
   display: flex;
   align-items: center;
@@ -18,12 +13,12 @@ const Container = styled.div``
 const Title = styled.h1``
 const Subtitle = styled.p``
 
-const Header: React.FC<Props> = ({ title, subtitle }) => (
+const Header = () => (
   <Head>
     <Link to="/">
       <Container>
-        <Title>{title}</Title>
-        <Subtitle>{subtitle}</Subtitle>
+        <Title>Kristian Sakarisson</Title>
+        <Subtitle>software engineer</Subtitle>
       </Container>
     </Link>
   </Head>
