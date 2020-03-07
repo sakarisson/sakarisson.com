70dd20dd4fe02dcfbbd65e950bacc5243a64f002
Kristian Sakarisson <kristian@sakarisson.com>
2020-03-07 21:37:31 +0200

    Add link in header

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,6 +1,12 @@
 import React from "react"
+import { Link } from "gatsby"
 import styled from "styled-components"
+
 import * as Typography from "./Typography"
+
+const StyledLink = styled(Link)`
+  text-decoration: none;
+`
 
 const Head = styled.header`
   display: flex;
@@ -13,10 +19,12 @@ const Container = styled.div``
 
 const Header = () => (
   <Head>
-    <Container>
-      <Typography.Title>Kristian Sakarisson</Typography.Title>
-      <Typography.Body>software engineer</Typography.Body>
-    </Container>
+    <StyledLink to="/">
+      <Container>
+        <Typography.Title>Kristian Sakarisson</Typography.Title>
+        <Typography.Body>software engineer</Typography.Body>
+      </Container>
+    </StyledLink>
   </Head>
 )
 
