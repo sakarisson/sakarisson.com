3a03612ba5af0661aaf979ed49a1f2269cb7dd05
Kristian Sakarisson <kristian@sakarisson.com>
2020-04-05 12:44:59 +0300

    Add portrait

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,9 @@
 import React from "react"
-import { Link } from "gatsby"
+import { Link, graphql, useStaticQuery } from "gatsby"
 import styled from "styled-components"
+import Img, { FixedObject } from "gatsby-image"
 
+import { File } from "../generated/graphql"
 import * as Typography from "./Typography"
 import Color from "../style/Color"
 
@@ -27,17 +29,60 @@ const Subtitle = styled(Typography.Body)`
   color: ${Color.TEXT_SECONDARY};
 `
 
-const Container = styled.div``
+const Container = styled.div`
+  display: grid;
+  grid-auto-flow: column;
+  padding: 0em 2em;
+`
 
-const Header = () => (
-  <Head>
-    <StyledLink to="/">
-      <Container>
-        <Title>Kristian Sakarisson</Title>
-        <Subtitle>software engineer in Helsinki</Subtitle>
-      </Container>
-    </StyledLink>
-  </Head>
-)
+const LeftContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  flex-direction: column;
+  justify-content: center;
+  padding-right: 2em;
+`
+
+const RoundImage = styled(Img)`
+  border-radius: 50%;
+`
+
+const query = graphql`
+  query {
+    portrait: file(relativePath: { eq: "me_portrait.jpg" }) {
+      childImageSharp {
+        fixed(width: 350, height: 350) {
+          ...GatsbyImageSharpFixed
+        }
+      }
+    }
+  }
+`
+
+type QueryData = {
+  portrait: File
+}
+
+const Header = () => {
+  const data = useStaticQuery<QueryData>(query)
+
+  const portraitFixed = data.portrait.childImageSharp?.fixed as FixedObject
+
+  return (
+    <Head>
+      <StyledLink to="/">
+        <Container>
+          <LeftContainer>
+            <div>
+              <Title>Kristian Sakarisson</Title>
+              <Subtitle>software engineer in Helsinki</Subtitle>
+            </div>
+          </LeftContainer>
+          <RoundImage fixed={portraitFixed} />
+        </Container>
+      </StyledLink>
+    </Head>
+  )
+}
 
 export default Header
