0219c2ccf70f9090d7c7aa2e0388bfa04ddaa56c
Kristian Sakarisson <kristian.sakarisson@wolt.com>
2021-02-28 11:21:56 +0200

    Reorganize metadata

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,24 +1,20 @@
 import { GetStaticPaths, GetStaticProps, NextPage } from 'next';
 import React from 'react';
 import matter from 'gray-matter';
+import { Subheading, Title } from '../../src/components/Typography';
 
 type Props = {
-  postContents: { title: string; date: string };
+  postMetadata: { title: string; date: string };
 };
 
-const PostTemplate: NextPage<Props> = ({ postContents }) => (
+const PostTemplate: NextPage<Props> = ({ postMetadata }) => (
   <div>
-    <div>This is a post</div>
-    <div>{postContents.title}</div>
+    <Title>{postMetadata.title}</Title>
+    <Subheading>{postMetadata.date}</Subheading>
   </div>
 );
 
 export const getStaticPaths: GetStaticPaths = async () => {
-  // Get the paths we want to pre-render based on users
-  // const paths = sampleUserData.map((user) => ({
-  //   params: { id: user.id.toString() },
-  // }))
-
   const paths = [
     {
       params: {
@@ -27,25 +23,22 @@ export const getStaticPaths: GetStaticPaths = async () => {
     },
   ];
 
-  // We'll pre-render only these paths at build time.
-  // { fallback: false } means other routes should 404.
   return { paths, fallback: false };
 };
 
-export const getStaticProps: GetStaticProps = async (context) => {
+export const getStaticProps: GetStaticProps<Props> = async (context) => {
   const slug = context.params?.slug;
 
   try {
-    // @ts-ignore
     const rawData = await import(`../../src/_posts/${slug}.md`);
 
-    const postContents = matter(rawData.default).data;
+    const postMetadata = matter(rawData.default).data;
 
     return {
-      props: { postContents },
+      props: { postMetadata },
     };
   } catch (error) {
-    return { props: { postContents: error.message } };
+    return { props: { postMetadata: error.message } };
   }
 };
 
