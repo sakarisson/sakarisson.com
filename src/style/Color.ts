5f7d1fdd6b6cb608b7fd6ea2ef3ee314c57bdc4d
Kristian Sakarisson <kristian.sakarisson@wolt.com>
2021-05-17 16:10:03 +0300

    Make background slightly lighter

--- src/style/Color.ts
+++ src/style/Color.ts
@@ -3,7 +3,7 @@ enum Color {
   TEXT_SECONDARY = 'rgb(125, 125, 125)',
   TEXT_HIGHLIGHT = '#00b8a9',
 
-  BACKGROUND = '#fbfcf7',
+  BACKGROUND = '#fbfcfa',
 
   CODE_BACKGROUND = 'rgba(0, 0, 0, 0.15)',
 }
